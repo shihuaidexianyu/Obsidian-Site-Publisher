9bdb2f41d2940d1c6cbacdde87c2be31e400fe65
shihuaidexianyu <exqinhongwei@outlook.com>
2026-03-18 17:48:49 +0800

    feat: update ESLint configuration to ignore .obsidian-plugin-build directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default [
     ignores: [
       "**/*.d.ts",
       "**/.generated/**",
+      "**/.obsidian-plugin-build/**",
       "**/.osp/**",
       "**/dist/**",
       "**/node_modules/**",
